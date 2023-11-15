011de7be2655983ec6f1f2c5f0420c78cead98e7
BenFloresMX <benfloresMX@outlook.com>
2023-11-14 23:57:15 -0600

    Learning Query Parameters and adding a new movie

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -22,6 +22,14 @@ movies = [
 		"year": "2009",
 		"rating": 7.8,
 		"category": "Acción"
+	},
+    {
+		"id": 3,
+		"title": "El Padrino",
+		"overview": "Don Vito Corleone es el respetado y temido jefe de una de las cinco ...",
+		"year": "1972",
+		"rating": 8.7,
+		"category": "Drama"
 	}
 ]
 
@@ -35,8 +43,12 @@ def get_movies():
     return movies
 
 @app.get('/movies/{id}', tags = ['movies'])
-def get_movie(id: int):
+def get_movie_id(id: int):
     for item in movies:
         if item["id"] == id:
             return item
     return id
+
+@app.get('/movies/', tags = ['movies'])
+def get_movies_by_category(category: str, year: int):
+    return list(filter(lambda item: item['category'] == category , movies))
