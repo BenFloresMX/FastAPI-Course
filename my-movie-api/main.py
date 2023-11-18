082f94a90f292e6a7b98efab8c4b6821c2cea2db
BenFloresMX <benfloresMX@outlook.com>
2023-11-18 02:30:08 -0600

    Learning to generate tokens with pyjwt library

--- my-movie-api/main.py
+++ my-movie-api/main.py
@@ -2,10 +2,15 @@ from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from jwt_manager import create_token
 
 app = FastAPI()
 app.title = 'Application with FastAPI'
 app.version = '0.0.1'
+
+class User(BaseModel):
+	email:str
+	password:str
 
 class Movie(BaseModel):
 	id: Optional[int] = None # Another way to express this is id: int | None = None
@@ -61,6 +66,10 @@ movies = [
 @app.get('/', tags =['home'])
 def message():
     return HTMLResponse('<h1> Hello World <h1/>')
+
+@app.post('/login', tags= ['auth'])
+def login(user: User):
+	return user 
 
 @app.get('/movies', tags = ['movies'], response_model= List[Movie], status_code=200)
 def get_movies() -> List[Movie]:
